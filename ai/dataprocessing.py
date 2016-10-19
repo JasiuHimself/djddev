16e0169200df1c4d5d1881394c8f6f1796e2477a
JasiuHimself <194959@student.pwr.edu.pl>
2016-10-19 08:51:03 +0200

    readme + uupdate

--- ai/dataprocessing.py
+++ ai/dataprocessing.py
@@ -1,5 +1,5 @@
 import string
-from scipy import signal
+from scipy.signal import butter, lfilter
 import matplotlib.pyplot as plt
 
 def column(matrix, i):
@@ -19,18 +19,36 @@ dataFile.close
 
 time = column(vector,0)
 accX = column(vector,1)
-accXFiltered = accX
+
 # accY = column(vector,2)
 # accZ = column(vector,3)
 
 
-b, a = signal.butter(4, 100, 'low', analog=True)
-w, h = signal.freqs(b, a)
-plt.plot(w, 20 * np.log10(abs(h)))
+# 200 hz
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
 
-plt.plot(time, accX);
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
 
+# Filter requirements.
+order = 10
+fs = 199      # sample rate, Hz
+cutoff = 15 # desired cutoff frequency of the filter, Hz
+
+# Get the filter coefficients so we can check its frequency response.
+b, a = butter_lowpass(cutoff, fs, order)
+
+
+accXFiltered = butter_lowpass_filter(accX,cutoff,fs,order)
+plt.plot(time,accXFiltered,"g", time,accX,"r")
+plt.plot();
 
 
 
---
+++ ai/readme.txt
@@ -0,0 +1,20 @@
+Co jest zrobione:
+
+-wczytanie danych z pliku
+-filtracja filtrem Butterwortha 10 rzędu
+
+
+do zrobienia
+obliczenie normy ze względnu na brak znaczenia orientacji
+
+
+
+miscellaneous
+wyższe częstotliwości mogą być wykorzystane do wykrywania np jazdy
+samochodem
+
+
+
+do zrobienia potem:
+
+    behavioral deep learning :D
