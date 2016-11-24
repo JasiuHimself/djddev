5d34ffc0b2a0d5c9d581227ac5f6f406267bd70a
Dawid Perdek <perdekdawid@gmail.com>
2016-11-24 23:11:48 +0100

    results in csv, mean of transport info added

--- android/SensorDataCollector/app/src/main/java/com/example/dyzio/sensordatacollector/CollectorActivity.java
+++ android/SensorDataCollector/app/src/main/java/com/example/dyzio/sensordatacollector/CollectorActivity.java
@@ -13,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,8 +26,10 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
     private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 112;
     private static String LOG_TAG = "SensorDataCollectorLog";
     private static String DIR_NAME = "SensorDataCollector";
-    public static int SAMPLING_RATE = 20000;        // time for sample in us
-    public static int TIMESTAMP_DIVIDER = 1000000;  // convert timestamp from ns to ms
+    private static int SAMPLING_RATE = 20000;        // time for sample in us
+    private static int TIMESTAMP_DIVIDER = 1000000;  // convert timestamp from ns to ms
+    private static String STANDBY_MESSAGE = "Choose your mean of transport to start collecting data.";
+    private static String COLLECTING_DATA_MESSAGE = "Collecting data... To stop click on selected mean of transport.";
 
     private File accelerometerFile = null;
     private File gyroscopeFile = null;
@@ -34,11 +39,26 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
     private Sensor gyroscope = null;
     private boolean collectingData = false;
     private long firstTimestamp = 0;
+    private String meanOfTransport = "";
+
+    private TextView message;
+    private Button bus;
+    private Button car;
+    private Button train;
+    private Button tram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collector);
+
+        message = (TextView) findViewById(R.id.textViewMessage);
+        bus = (Button) findViewById(R.id.buttonCollectBus);
+        car = (Button) findViewById(R.id.buttonCollectCar);
+        train = (Button) findViewById(R.id.buttonCollectTrain);
+        tram = (Button) findViewById(R.id.buttonCollectTram);
+
+        message.setText(STANDBY_MESSAGE);
 
         if (Build.VERSION.SDK_INT >= 23) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE);
@@ -61,7 +81,7 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
         }
 
         if (isExternalStorageWritable) {
-            File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), DIR_NAME);
+            File dir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), DIR_NAME);
             if (!dir.exists()) {
                 dir.mkdirs();
                 Log.d(LOG_TAG, "Directory created.");
@@ -69,7 +89,7 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
                 Log.d(LOG_TAG, "Directory already exists.");
             }
             if (accelerometer != null) {
-                accelerometerFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + DIR_NAME, "accelerometer.txt");
+                accelerometerFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + DIR_NAME, "accelerometer.csv");
                 if (!accelerometerFile.exists()) {
                     try {
                         accelerometerFile.createNewFile();
@@ -82,7 +102,7 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
                 }
             }
             if (gyroscope != null) {
-                gyroscopeFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + DIR_NAME, "gyroscope.txt");
+                gyroscopeFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/" + DIR_NAME, "gyroscope.csv");
                 if (!gyroscopeFile.exists()) {
                     try {
                         gyroscopeFile.createNewFile();
@@ -97,15 +117,61 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
         }
     }
 
-    public void onButtonCollectDataClick(View v) {
+    private void toggleCollectingData() {
         collectingData = !collectingData;
 
         if (collectingData) {
             Log.d(LOG_TAG, "Starting collecting data.");
+            message.setText(COLLECTING_DATA_MESSAGE);
         } else {
             Log.d(LOG_TAG, "Stopping collecting data.");
+            message.setText(STANDBY_MESSAGE);
             firstTimestamp = 0;
         }
+    }
+
+    public void onButtonCollectBusClick(View v) {
+        toggleCollectingData();
+
+        int visibility = collectingData ? Button.INVISIBLE : Button.VISIBLE;
+        car.setVisibility(visibility);
+        train.setVisibility(visibility);
+        tram.setVisibility(visibility);
+
+        meanOfTransport = "BUS";
+    }
+
+    public void onButtonCollectCarClick(View v) {
+        toggleCollectingData();
+
+        int visibility = collectingData ? Button.INVISIBLE : Button.VISIBLE;
+        bus.setVisibility(visibility);
+        train.setVisibility(visibility);
+        tram.setVisibility(visibility);
+
+        meanOfTransport = "CAR";
+    }
+
+    public void onButtonCollectTrainClick(View v) {
+        toggleCollectingData();
+
+        int visibility = collectingData ? Button.INVISIBLE : Button.VISIBLE;
+        bus.setVisibility(visibility);
+        car.setVisibility(visibility);
+        tram.setVisibility(visibility);
+
+        meanOfTransport = "TRAIN";
+    }
+
+    public void onButtonCollectTramClick(View v) {
+        toggleCollectingData();
+
+        int visibility = collectingData ? Button.INVISIBLE : Button.VISIBLE;
+        bus.setVisibility(visibility);
+        car.setVisibility(visibility);
+        train.setVisibility(visibility);
+
+        meanOfTransport = "TRAM";
     }
 
     public void onButtonClearDataClick(View v) {
@@ -117,6 +183,8 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
             gyroscopeFile.delete();
             Log.d(LOG_TAG, "Gyroscope data file deleted.");
         }
+
+        Toast.makeText(this, "Data deleted.", Toast.LENGTH_SHORT).show();
     }
 
     public void onButtonSendDataClick(View v) {
@@ -127,25 +195,25 @@ public class CollectorActivity extends AppCompatActivity implements SensorEventL
     public void onSensorChanged(SensorEvent event) {
         if (collectingData) {
             if (firstTimestamp == 0) {
-                firstTimestamp = Math.round((double)event.timestamp/TIMESTAMP_DIVIDER);   // convert timestamp to milliseconds
+                firstTimestamp = Math.round((double)event.timestamp/TIMESTAMP_DIVIDER);
             }
             if (event.sensor.equals(accelerometer)) {
                 try {
                     FileWriter fileWriter = new FileWriter(accelerometerFile, true);
-                    fileWriter.append(Math.round((double)event.timestamp/TIMESTAMP_DIVIDER) - firstTimestamp + "," + event.values[0] + "," + event.values[1] + "," + event.values[2] + System.getProperty("line.separator"));
+                    fileWriter.append(meanOfTransport + "," + (Math.round((double)event.timestamp/TIMESTAMP_DIVIDER) - firstTimestamp) + "," + event.values[0] + "," + event.values[1] + "," + event.values[2] + System.getProperty("line.separator"));
                     fileWriter.flush();
                     fileWriter.close();
                 } catch (IOException e) {
-                    Log.e(LOG_TAG, "File write failed: " + e.toString());
+                    Log.e(LOG_TAG, "Write to file failed: " + e.toString());
                 }
             } else if (event.sensor.equals(gyroscope)) {
                 try {
                     FileWriter fileWriter = new FileWriter(gyroscopeFile, true);
-                    fileWriter.append(Math.round((double)event.timestamp/TIMESTAMP_DIVIDER) - firstTimestamp + "," + event.values[0] + "," + event.values[1] + "," + event.values[2] + System.getProperty("line.separator"));
+                    fileWriter.append(meanOfTransport + "," + (Math.round((double)event.timestamp/TIMESTAMP_DIVIDER) - firstTimestamp) + "," + event.values[0] + "," + event.values[1] + "," + event.values[2] + System.getProperty("line.separator"));
                     fileWriter.flush();
                     fileWriter.close();
                 } catch (IOException e) {
-                    Log.e(LOG_TAG, "File write failed: " + e.toString());
+                    Log.e(LOG_TAG, "Write to file failed: " + e.toString());
                 }
             }
         }
