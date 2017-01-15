da42e59759c3abd8cc592b0bd7cd1ac7b1f95776
JasiuHimself <194959@student.pwr.edu.pl>
2017-01-16 00:18:57 +0100

    extracted features: mean, variance

--- ai/dataprocessing.py
+++ ai/dataprocessing.py
@@ -3,6 +3,7 @@
 
 import string
 import numpy as np
+import csv
 from scipy.signal import butter, lfilter
 from scipy.fftpack import fft
 import matplotlib.pyplot as plt
@@ -33,99 +34,127 @@ def normWithoutDC(norm):
 #aka srednia arytmetyczna
 def signalEnergy(signal):
     sumator = 0
-    for n in range(0,N):
-        print sumator
+    for n in range(0,len(signal)):
         sumator += signal[n]
-    return (1.0/N)*sumator
+    return (1.0/len(signal))*sumator
 
 def signalVariance(signal):
     sumator = 0
-    meanValue = signalEnergy(signal,N) #signal mean value
-    for n in range(0, N):
+    meanValue = signalEnergy(signal) #signal mean value
+    for n in range(0, len(signal)):
         sumator += pow((signal[n] - meanValue),2)
-    return (1.0/N)*sumator
+    return (1.0/len(signal))*sumator
 
+def fft(signal):
+    return np.fft.fft(signal)
 
-def okienko(signal,windowWidth, overlappingPercent = 0, currentPositionInSignal = 0):
+def window(signal,windowWidth, overlappingPercent = 0, currentPositionInSignal = 0):
+    overlapping = 0
     if (overlappingPercent):
         overlapping = (int)(overlappingPercent/100.0*windowWidth)
     windowBegining = currentPositionInSignal * windowWidth - currentPositionInSignal * overlapping
     if (windowBegining + windowWidth <= len(signal)):
         window = signal[windowBegining:windowBegining+windowWidth]
-        return window, currentPositionInSignal+1
+        return window #, currentPositionInSignal+1
     else:
         return False
 
-
-
-def createTimeVectorToPlotWindow(windowWidth, overlappingPercent, currentPositionInSignal):
+def createTimeVectorToPlotWindow(windowWidth, overlappingPercent, currentPositionInSignal =0):
+    overlapping = 0
     if (overlappingPercent):
         overlapping = (int)(overlappingPercent/100.0*windowWidth)
     windowBegining = currentPositionInSignal * windowWidth - currentPositionInSignal * overlapping
     return range(windowBegining,windowBegining+windowWidth)
 
-
+def signalRangeInWindowsCount(signal, windowWidth, overlappingPercent):
+    overlapping = 0
+    windowsCount = 0
+    windowBegining = 0
+    if (overlappingPercent):
+        overlapping = (int)(overlappingPercent/100.0*windowWidth)
+    while(windowBegining + windowWidth <= len(signal)):
+        windowsCount += 1
+        windowBegining += windowWidth - overlapping
+    return windowsCount
 
 def readDataFile(dataFileName):
-    dataFile = open(dataFileName, 'r')
-    vector = []
-    while True:
-        line = dataFile.readline();
-        if not line: break
-        line = line.rstrip()
-        vector.append([float(x) for x in line.split(',')])
-    dataFile.close
-    return vector
+    csv_in = open(dataFileName, 'rb')
+    myreader = csv.reader(csv_in)
 
+    sample_Class_vector = []
+    accX_vector = []
+    accY_vector = []
+    accZ_vector = []
+    gyrR_vector = []
+    gyrP_vector = []
+    gyrY_vector = []
 
+    for row in myreader:
+        sample_Class, accX, accY, accZ, gyrR, gyrP, gyrY = row
+        sample_Class_vector, accX_vector, accY_vector, accZ_vector, gyrR_vector, gyrP_vector, gyrY_vector = zip(*myreader)
+        accX_vector = map(float, accX_vector)
+        accY_vector = map(float, accY_vector)
+        accZ_vector = map(float, accZ_vector)
+        gyrR_vector = map(float, gyrR_vector)
+        gyrP_vector = map(float, gyrP_vector)
+        gyrY_vector = map(float, gyrY_vector)
+
+    return sample_Class_vector, accX_vector, accY_vector, accZ_vector, gyrR_vector, gyrP_vector, gyrY_vector
 
 
 
 #otworz plik i zapisz wartosc do wektora
-vector = readDataFile('accgyr.csv');
+sample_Class,accX,accY,accZ,gyrR,gyrP,gyrY = readDataFile('accgyrclass.csv');
+
+windowWidth = 2
+overlappingPercent = 0
 
 
 
-#sample_Class = column(vector,0)
-accX = column(vector,0)
-accY = column(vector,1)
-accZ = column(vector,2)
-gyrR = column(vector,3)
-gyrP = column(vector,4)
-gyrY = column(vector,5)
-accNorm = norm(accX,accY,accZ)
-gyrNorm = norm(gyrR,gyrP,gyrY)
-accNormDC = normWithoutDC(accNorm)
-gyrNormDC = normWithoutDC(gyrNorm)
+accNormDC = normWithoutDC(norm(accX,accY,accZ))
+gyrNormDC = normWithoutDC(norm(gyrR,gyrP,gyrY))
+
 
 
 plt.figure()
-
 plt.subplot(2,1,1)
 plt.hold(True)
 plt.grid(True)
-plt.plot(accX)
-plt.plot(accY)
-plt.plot(accZ)
-plt.plot(accNorm,'.')
-plt.plot(accNormDC,'x')
 
-typyPlotu = ['x', 'o', '*', '+', 's', 'd', 'v', '^', '<']
+plt.plot(accNormDC)
 
-for i in range(0,9):
-    wycinek, pozycja = okienko(accX,10,50,i)
-    czasik = createTimeVectorToPlotWindow(10,50,i)
-    plt.plot(czasik,wycinek, typyPlotu[i])
 
+# Zaznacznie dzialania okienek
+#
+# typyPlotu = ['x', 'o', '*', '+', 's', 'd', 'v', '^', '<']
+#
+# for i in range(0,9):
+#     wycinek, pozycja = window(accNormDC,10,50,i)
+#     czasik = createTimeVectorToPlotWindow(10,50,i)
+#     plt.plot(czasik,wycinek, typyPlotu[i])
+
+# plotowanie energii
+# for i in range(0, signalRangeInWindowsCount(accNormDC, windowWidth, overlappingPercent)):
+#     energy = signalEnergy(window(accNormDC,windowWidth,overlappingPercent,i))
+#     energyWindow = np.empty(windowWidth);
+#     energyWindow.fill(energy)
+#     plt.plot(createTimeVectorToPlotWindow(windowWidth,overlappingPercent,i),energyWindow,'g-')
+
+# plotowanie wariancji
+# for i in range(0, signalRangeInWindowsCount(accNormDC, windowWidth, overlappingPercent)):
+#     variance = signalVariance(window(accNormDC,windowWidth,overlappingPercent,i))
+#     print variance
+#     varianceWindow = np.empty(windowWidth);
+#     varianceWindow.fill(variance)
+#     plt.plot(createTimeVectorToPlotWindow(windowWidth,overlappingPercent,i),varianceWindow,'r-')
 
 # GYROSCOPE
 
 plt.subplot(2,1,2)
 plt.hold(True)
-plt.plot(gyrR)
-plt.plot(gyrP)
-plt.plot(gyrY)
-plt.plot(gyrNorm,'.')
-plt.plot(gyrNormDC,'x')
+
+plt.plot(gyrNormDC)
+
+# plotowanie wariancji
 
 plt.show()
--- ai/readme.txt
+++ ai/readme.txt
@@ -10,9 +10,8 @@ Co jest zrobione:
 
 -zostało ogarnięte, że jak jest range (0,N) to znaczy for i=0;i<=N-1;i++, generalnie wtf
 -wczytanie danych z pliku
--filtracja filtrem Butterwortha 10 rzędu
--obliczenie normy ze względnu na brak znaczenia orientacji - nie mam jak tego zrobić bo
--usuwanie dc component
+(-filtracja filtrem Butterwortha 10 rzędu)
+-obliczenie normy ze względnu na brak znaczenia orientacji
 
 
 
@@ -22,13 +21,11 @@ co trzeba zainstalować, żeby zainstalować theano i kerasa?
 
 sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 
-sudo pip install theano 
+sudo pip install theano
 sudo pip install keras
 i jest ok
 
 
-WALIDACJA USUWANIA DC COMPONENT!!!!!!!!!!!!!
-WYDAJE SIE ŻE COŚ NIE DO KOŃCA DZIAŁA, ALE PRZEKLEPAŁEM WZÓR
 
 the gyroscope energy Es˜ω,
 the accelerometer energy Es˜a,
@@ -47,3 +44,11 @@ samochodem
 CELE
     żeby działało na sieciach neuronowych
     behavioral deep learning :D
+
+
+
+
+
+
+z acc x,y,z i gyro r,p,y tworzę wektory normWithoutDC dla każdego z osobna
+feature extraction is realised for these created vector.
